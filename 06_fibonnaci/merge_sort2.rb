33fa746357635aab0b0d691750d0fcf3c3963307
Rafal Rosa <dark0mlet@gmail.com>
2017-03-21 20:04:37 +0000

    Fucking made it!

--- 06_fibonnaci/merge_sort2.rb
+++ 06_fibonnaci/merge_sort2.rb
@@ -1,18 +1,14 @@
 require 'pry'
 
 def merge_sort(arr)
-    if arr.length < 2
+    if arr.length == 1
         return arr
     end
     mid = arr.length/2 - 1
     left_side = arr[0..mid]
     right_side = arr[mid + 1..-1]
-    merge_sort(left_side)
-    merge_sort(right_side)
-    if left_side.length == 1 && right_side.length == 1
-        p merge_arrays(left_side,right_side)
-    end
-    
+
+    merge_arrays(merge_sort(left_side), merge_sort(right_side))
 end
 
 def merge_arrays(left, right)
@@ -36,8 +32,7 @@ def merge_arrays(left, right)
         work_array << right[j]
         j += 1
     end
-    p left,right
     return work_array
 end
 
-merge_sort([1,2,3,2,32,4,2,42,5,65,22,31,23])
+p merge_sort([1,2,3,2,32,4,5,535,34,32,421,43,24,235,235,342,54,345,235,2,42,5,65,22,31,23])
